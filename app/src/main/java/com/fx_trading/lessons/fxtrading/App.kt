a663c845a7d435761af8b14a2136eafa3c5bad0f
Viktor <morgachev.v.s@mail.ru>
2022-03-01 15:01:45 +0600

    Пофиксил баги

--- app/src/main/java/com/fx_trading/lessons/fxtrading/App.kt
+++ app/src/main/java/com/fx_trading/lessons/fxtrading/App.kt
@@ -3,9 +3,10 @@ package com.fx_trading.lessons.fxtrading
 import android.app.Application
 import android.content.Context
 import com.fx_trading.lessons.fxtrading.di.component.AppComponent
-import com.fx_trading.lessons.fxtrading.di.DaggerAppComponent
+import com.fx_trading.lessons.fxtrading.di.component.DaggerAppComponent
 
 class App : Application() {
+
 
     val appComponent: AppComponent by lazy {
         DaggerAppComponent.builder()
